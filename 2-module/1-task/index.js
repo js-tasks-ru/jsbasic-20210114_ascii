c92b8bc23a83c5d456ad64e8daed6180544bbc9d
Matvey Procenko <benderisgreat32@gmail.com>
2021-01-25 23:10:59 +0200

    Solved 2.1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -7,8 +7,9 @@
 function sumSalary(salaries) {
   let sum = 0;
   for (let key in salaries) {
-    if (typeof salaries[key] == 'number' && isNaN(salaries[key]) == false && isFinite(salaries[key] == true)) {
-      sum += salaries[key];
+    let item = salaries[key];
+    if (typeof item == 'number' && isNaN(item) == false && isFinite(item)) {
+      sum += item;
     }
   }
   return sum;
